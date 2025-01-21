c5b397546b4f3c5cc5068c20b2c9aa056277da4b
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-21 12:48:49 -0700

    changing title

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Hangman from "@/app/ui/hangman/hangman";
 export default function Home() {
   return (
     <main>
-      <div className="text-6xl font-bold">
-        Hangman!
-        <Hangman/>
-      </div>
+      <h1 className='text-6xl font-bold'>
+      Daily Hangman!
+      </h1>
+      <Hangman/>
     </main>
   );
 }

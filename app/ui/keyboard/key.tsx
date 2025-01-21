11a16b0af0aaa0cb1487857dede2f6c122a49e2b
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-21 11:03:37 -0700

    Added a key component that has a callback and can color itself depending on it's assigned state

---
+++ app/ui/keyboard/key.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export enum KeyState {
+	NotGuessed,
+	Correct,
+	Incorrect
+};
+
+export default function Key({
+	character,
+	clickCallback,
+	keyState = KeyState.NotGuessed,
+} : {
+	character: string;
+	clickCallback: (character: string) => void;
+	keyState?: KeyState;
+}) {
+	const [bgColor, setBgColor] = useState('');
+
+	useEffect(() => {
+		switch (keyState) {
+			case KeyState.Correct:
+				setBgColor('bg-blue-300');
+				break;
+			case KeyState.Incorrect:
+				setBgColor('bg-red-800');
+				break;
+			// Fallthrough intended
+			case KeyState.NotGuessed:
+			default:
+				setBgColor('');
+				break;
+		}
+	}, [keyState])
+
+	return (
+		<div
+			className={`relative flex flex-col w-16 h-16 items-center border-x-4 border-t-2 border-b-8 rounded-md border-slate-900 dark:border-white ${bgColor}`}
+			onClick={() => clickCallback(character)}
+		>
+				<span className="uppercase text-3xl m-auto text-center hover:cursor-pointer select-none">
+					{character}
+				</span>
+				<div className="absolute bottom-1 w-11/12 h-0.5 bg-slate-900 dark:bg-white rounded-sm" />
+		</div>
+	)
+}

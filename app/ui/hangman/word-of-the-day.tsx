de4666cec4e4309eff47a1a5119fa362ec9cb4c9
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-21 19:29:58 -0700

    fixed the dots not showing up in the word ofthe day on large screens

--- app/ui/hangman/word-of-the-day.tsx
+++ app/ui/hangman/word-of-the-day.tsx
@@ -9,9 +9,9 @@ export default function WordOfTheDay({
 		<div className="flex flex-col gap-8">
 			<div className="flex flex-col sm:flex-row justify-center items-center gap-6">
 				<span className="font-light md:font-extralight text-2xl md:text-6xl margin-auto">{data.word}</span>
-				<span className="md:text-2xl hidden sm:visible">•</span>
+				<span className="md:text-2xl hidden sm:inline">•</span>
 				<span className="text-xl md:text-4xl font-extralight md:font-thin text-center">{data.pronunciation}</span>
-				<span className="md:text-2xl hidden sm:visible">•</span>
+				<span className="md:text-2xl hidden sm:inline">•</span>
 				<span className="leading-snug md:text-2xl font-normal md:font-light dark:text-slate-100 text-slate-600">{data.wordClass}</span>
 			</div>
 			<p className="md:text-2xl">{data.definition}</p>

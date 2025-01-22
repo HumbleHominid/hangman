e3068ebc7664bc466810abc95934fb9389a038f0
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-21 19:16:49 -0700

    adjusting key sizes for mobile

--- app/ui/keyboard/key.tsx
+++ app/ui/keyboard/key.tsx
@@ -37,13 +37,13 @@ export default function Key({
 
 	return (
 		<div
-			className={`relative flex flex-col transition-all w-16 h-16 items-center border-x-4 border-t-2 border-b-8 md:hover:-translate-y-1 rounded-md border-slate-900 dark:border-white ${bgColor} md:hover:cursor-pointer active:translate-y-1`}
+			className={`relative flex flex-col transition-all w-12 h-12 md:w-16 md:h-16 items-center border-x-2 border-t border-b-4 md:border-x-4 md:border-t-2 md:border-b-8 md:hover:-translate-y-1 rounded-md border-slate-900 dark:border-white ${bgColor} md:hover:cursor-pointer active:translate-y-1`}
 			onClick={() => clickCallback(character)}
 		>
 				<span className="uppercase text-3xl m-auto text-center select-none">
 					{character}
 				</span>
-				<div className="absolute bottom-1 w-9/12 h-0.5 bg-slate-900 dark:bg-white rounded-sm" />
+				<div className="absolute bottom-0.5 md:bottom-1 w-9/12 h-0.5 bg-slate-900 dark:bg-white rounded-sm" />
 		</div>
 	)
 }

c7be83742f9f972d7395e99e14069c045793a994
HumbleHominid <michael.d.fryer@gmail.com>
2025-01-23 09:17:31 -0700

    Adding a date to the title

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hangman from "@/app/ui/hangman/hangman";
+import { format } from 'date-fns';
 
 export default function Home() {
+  const date = format(new Date(), 'eeee do MMM u')
   return (
     <main className="overflow-hidden">
-      <h1 className='text-6xl font-light mb-4 underline decoration-2 underline-offset-2'>
-      Your Daily Hangman Fix
+      <h1 className="mb-2 text-6xl font-light underline decoration-2 underline-offset-2">
+      Daily Hangman
       </h1>
+      <p className="mb-4 tracking-wide text-2xl">{date}</p>
       <Hangman/>
     </main>
   );

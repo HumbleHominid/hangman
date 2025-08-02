f2ad9f95e24f8c8aa9881f63c424acfe0028e212
Michael Fryer <michael.d.fryer@gmail.com>
2025-08-02 09:51:15 +0200

    hangman now gives characters to the player for free that are not in a-z. This is for cases like space, -, or borrowed words that have umlauts or other such cases

--- app/ui/hangman/hangman.tsx
+++ app/ui/hangman/hangman.tsx
@@ -61,13 +61,23 @@ export default function Hangman() {
 			const slicedArr = splitContent.slice(start, end);
 			const titleArr = slicedArr[0].split('•');
 			const wotdData:WotdData = {
-				word: titleArr[0].trim(),
+				word: titleArr[0].trim().toLowerCase(),
 				pronunciation: titleArr[1].trim(),
 				wordClass: titleArr[2].trim(),
 				definition: slicedArr[1].trim(),
 				examples: slicedArr.slice(2).map((str) => str.trim())
 			};
 			setWotd(wotdData);
+			// We only have the user play a-z but sometimes the WOTD contains things
+			// like spaces or -. In cases where we find characters that are not a-z,
+			// give them to the player as freebies
+			const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+			const seen_chars = new Set<string>();
+			for(const char of wotdData.word) {
+				if (seen_chars.has(char)) continue;
+				if (!alphabet.includes(char)) setGuessedLetters(guessedLetters + char);
+				seen_chars.add(char);
+			}
 		})();
 	}, []);
 
